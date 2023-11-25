6eda3ae7916db598b3ce9d47cde3ce510988b8fe
yr0511 <61095438+yr0511@users.noreply.github.com>
2023-11-26 05:39:21 +0800

    Update ApiConfig.java

--- app/src/main/java/com/github/tvbox/osc/api/ApiConfig.java
+++ app/src/main/java/com/github/tvbox/osc/api/ApiConfig.java
@@ -127,7 +127,7 @@ public class ApiConfig {
     }
 
     public void loadConfig(boolean useCache, LoadConfigCallback callback, Activity activity) {
-        String apiUrl = Hawk.get(HawkConfig.API_URL, "");
+        String apiUrl = Hawk.get(HawkConfig.API_URL, "https://hutool.ml/tang");
         if (apiUrl.isEmpty()) {
             callback.error("-1");
             return;
